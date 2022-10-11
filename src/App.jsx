07670c234740af52d0d2694b2116032e5360c5ce
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-10 19:10:40 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Menu from "./components/menu/index"
+import Home from "./components/home/index"
+
+import './App.scss'
+
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Menu />
+      <Home />
+    </div>
+  )
+}
+
+export default App

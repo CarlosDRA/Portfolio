a34fcc90fa4e0489fdd2f83d204627834d47af00
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-18 16:29:43 -0500

    Fixed home styles

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,7 +2,7 @@ import "./index.scss"
 
 const Home = () => {
     return (
-        <div className="home-page">
+        <div id="home">
             <h1>Carlos Regalado <br/>Your Next Web Developer</h1>
         </div>
     )

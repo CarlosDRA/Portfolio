17a1cd7275fa72cd396fe5980eee2c9fcfb1f36f
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-18 16:28:35 -0500

    Updated menu sections

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -5,15 +5,15 @@ export default function Menu() {
     return (
         <nav className="nav-bar">
             <div className="nav-logo_container">
-                <a href="">
+                <a href="#home">
                     <img src="src/assets/logo.svg" alt="logo" />
                 </a>
             </div>
             <div className="nav-links">
                 <ul>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Work</a></li>
-                    <li><a href="">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
         </nav>

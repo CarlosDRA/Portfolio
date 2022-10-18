1c2f9f937131571edd602b62caefdf167043b661
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-18 16:33:22 -0500

    Created about component structure

---
+++ src/components/about/index.jsx
@@ -0,0 +1,24 @@
+import "./index.scss";
+
+export default function About (){
+    return(
+        <div id="about">
+            <div className="about_text-container">
+                <h2>Obtain the Best Results With The Best Practices</h2>
+                <p>I Will Create Your Dream Projects With An Ever Expanding Set Of Tools, Technologies And Knowledge</p>
+            </div>
+            <div className="about_tech-container">
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+                <div className="tech tech-logo"></div>
+            </div>
+        </div>
+    )
+}

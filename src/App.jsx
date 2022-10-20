d8f0b3825811ef24b3832fd8bbec73948e9e6b2b
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-20 12:21:24 -0500

    Updated Carousel functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/home/index";
 import About from "./components/about/index";
 import Work from "./components/work/index";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import './App.scss'
 
@@ -11,28 +11,40 @@ import './App.scss'
 function App() {
 
   const [currentIndex, setCurrentIndex] = useState(0);
-  const group = document.querySelectorAll(".card");
-
-
+  const [group, setGroup] = useState();
+  
+  useEffect(() => {
+    setGroup(document.querySelectorAll(".card"));
+  }, [currentIndex])
+  
   const handleRightClick = () => {
-      console.log(group);
-      const nextIndex = currentIndex + 1 <= group.length - 1 ? currentIndex + 1: 0;
+    const nextIndex = currentIndex - 1 < 0 ? 2: currentIndex - 1;
+    
+    const currentCard = document.querySelector(`[data-index="${currentIndex}"]`);
+    const nextCard = document.querySelector(`[data-index="${nextIndex}"]`);
+    console.log(currentIndex, currentCard);
+    console.log(nextIndex)
 
-      const currentCard = document.querySelector(`[data-index="${currentIndex}"]`);
-      const nextCard = document.querySelector(`[data-index="${nextIndex}"]`);
-      console.log(currentCard);
-      console.log(nextIndex)
+    currentCard.dataset.status = "after";
 
-      currentCard.dataset.status = "after";
+    nextCard.dataset.status = "active";
 
-      nextCard.dataset.status = "active";
-
-      setCurrentIndex(nextIndex);
-      //setCurrentIndex(index => index + 1);
+    setCurrentIndex(nextIndex);
   }
 
   const handleLeftClick = () => {
-      setCurrentIndex(index => index - 1);
+    const nextIndex = currentIndex + 1 <= group.length - 1 ? currentIndex + 1: 0;
+    
+    const currentCard = document.querySelector(`[data-index="${currentIndex}"]`);
+    const nextCard = document.querySelector(`[data-index="${nextIndex}"]`);
+    console.log(currentIndex, currentCard);
+    console.log(nextIndex)
+
+    currentCard.dataset.status = "after";
+
+    nextCard.dataset.status = "active";
+
+    setCurrentIndex(nextIndex);
   }
 
   return (
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,7 +2,11 @@ import "./index.scss";
 
 export default function Card(props){
     return(
-        <div className={props.className} data-index={props.index} data-status={props.status}>
+        <div 
+            className={props.className} 
+            data-index={props.index} 
+            data-status={props.status}
+        >
             <a href={props.url} target="_blank" rel="noopener noreferrer"></a>
             <a href={props.code} target="_blank" rel="noopener noreferrer"></a>
         </div>

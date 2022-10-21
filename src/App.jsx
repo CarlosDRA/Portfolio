bb3cb04bab1e39901b4ad8c42daa161a50a1698a
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-21 17:09:47 -0500

    Fixed carousel slides

--- src/App.jsx
+++ src/App.jsx
@@ -20,33 +20,35 @@ function App() {
   const handleRightClick = () => {
     const nextIndex = currentIndex + 1 <= group.length -1 ? currentIndex + 1: 0;
     const prevIndex = currentIndex - 1 < 0 ? group.length - 1 : currentIndex - 1;
+    const nextVisibleIndex = nextIndex + 1 <= group.length - 1? nextIndex + 1: 0;
     
     const currentCard = document.querySelector(`[data-index="${currentIndex}"]`);
     const nextCard = document.querySelector(`[data-index="${nextIndex}"]`);
-    const prevCard = document.querySelector(`[data-index="${prevIndex}"]`)
+    const prevCard = document.querySelector(`[data-index="${prevIndex}"]`);
+    const rightCard = document.querySelector(`[data-index="${nextVisibleIndex}"]`);
 
-    prevCard.dataset.status="right";
+    prevCard.dataset.status="hidden";
     currentCard.dataset.status = "left";
     nextCard.dataset.status = "active";
-
+    rightCard.dataset.status = "right"
+    
     setCurrentIndex(nextIndex);
   }
 
   const handleLeftClick = () => {
     const nextIndex = currentIndex - 1 < 0 ? group.length - 1 : currentIndex - 1;
     const prevIndex = currentIndex + 1 <= group.length -1 ? currentIndex + 1: 0;
-    
+    const nextVisibleIndex = nextIndex - 1 < 0 ? group.length - 1 : nextIndex - 1;
+
     const currentCard = document.querySelector(`[data-index="${currentIndex}"]`);
     const nextCard = document.querySelector(`[data-index="${nextIndex}"]`);
     const prevCard = document.querySelector(`[data-index="${prevIndex}"]`);
+    const leftCard = document.querySelector(`[data-index="${nextVisibleIndex}"]`);
 
-    prevCard.dataset.status="left";
+    prevCard.dataset.status="hidden";
     currentCard.dataset.status = "right";
     nextCard.dataset.status = "active";
-
-    console.log("Current index", currentIndex);
-    console.log("next index", nextIndex)
-    console.log("prevIndex", prevIndex);
+    leftCard.dataset.status = "left";
 
     setCurrentIndex(nextIndex);
   }
--- src/components/work/index.jsx
+++ src/components/work/index.jsx
@@ -7,9 +7,15 @@ export default function Work (props){
         <div id="work">
             <h2>Here Are Some Of The Projects I’ve Worked On</h2>
             <div className="carousel">
-                <Card className="card middle" index="0" status="active"/>
-                <Card className="card right"  index="1" status="right"/>
-                <Card className="card left"   index="2" status="left"/>
+                <Card className="card" index="0" status="active"/>
+                <Card className="card" index="1" status="right"/>
+                <Card className="card" index="2" status="hidden"/>
+                <Card className="card" index="3" status="hidden"/>
+                <Card className="card" index="4" status="hidden"/>
+                <Card className="card" index="5" status="hidden"/>
+                <Card className="card" index="6" status="hidden"/>
+                <Card className="card" index="7" status="hidden"/>
+                <Card className="card" index="8" status="left"/>
 
                 <button 
                     className="button button-right"

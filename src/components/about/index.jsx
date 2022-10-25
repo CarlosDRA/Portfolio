ec5506da526e659df9bf7b51d4afef6e7b5ebe9f
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-24 20:29:47 -0500

    Updated Icons

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -7,17 +7,23 @@ export default function About (){
                 <h2>Obtain the Best Results With The Best Practices</h2>
                 <p>I Will Create Your Dream Projects With An Ever Expanding Set Of Tools, Technologies And Knowledge</p>
             </div>
+
             <div className="about_tech-container">
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
-                <div className="tech tech-logo"></div>
+                <i className="devicon-html5-plain colored tech-logo"></i>
+                <i className="devicon-css3-plain colored tech-logo"></i>
+                <i className="devicon-sass-original colored tech-logo"></i>
+                <i className="devicon-javascript-plain colored tech-logo"></i>
+                <i className="devicon-nodejs-plain colored tech-logo"></i>
+                <i className="devicon-react-original colored tech-logo"></i>
+                <i className="devicon-mongodb-plain colored tech-logo"></i>
+                <i className="devicon-mysql-plain colored tech-logo"></i>
+                <i className="devicon-amazonwebservices-original colored tech-logo"></i>
+                <i className="devicon-figma-plain colored tech-logo"></i>
+                <i className="devicon-git-plain colored tech-logo"></i>
+                <i className="devicon-github-original tech-logo"></i>
+                <i className="devicon-bash-plain tech-logo"></i>
+                <i className="devicon-windows8-original colored tech-logo"></i>
+                <i className="devicon-ubuntu-plain colored tech-logo"></i>
             </div>
         </div>
     )

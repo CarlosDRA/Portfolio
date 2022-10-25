a89020fdd52975b677c8c14671f7b2121de90720
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-10-25 13:24:19 -0500

    Updated main page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,7 +3,17 @@ import "./index.scss"
 const Home = () => {
     return (
         <div id="home">
-            <h1>Carlos Regalado <br/>Your Next Web Developer</h1>
+            <h1>Carlos Regalado</h1>
+            <h2>Web Developer</h2>
+
+            <div className="buttons-container">
+                <a href="#work">Work</a>
+                <a href="#contact">Contact</a>
+            </div>
+
+            <div className="background-reference">
+                <a href="https://unsplash.com/photos/efJTdhsmzPI?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink" target="_blank">Photo</a> by <a href="https://unsplash.com/@yuhao?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Pang Yuhao</a> on <a href="https://unsplash.com/s/photos/city-at-night?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a>
+            </div>
         </div>
     )
 }
